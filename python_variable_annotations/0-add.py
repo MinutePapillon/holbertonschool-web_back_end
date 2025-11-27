d04c903fc6b51f963006ed139d982681232ae599
MinutePapillon <christophebarrere@gmail.com>
2025-11-27 15:14:13 +0100

    feat: add type-annotated add() function to sum two floats

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Module for add function"""
-
+"""
+Type-annotated function add
+"""
 
 def add(a: float, b: float) -> float:
-    """type-annotated function that takes a float a and a float b
-    as arguments and returns their sum as a float."""
+    """Return the sum of two floats."""
     return a + b
