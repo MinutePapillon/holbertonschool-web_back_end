8f084562e8b2d7f6630d983459eeebd88d5d8b6f
MinutePapillon <christophebarrere@gmail.com>
2025-11-27 15:11:29 +0100

    Write a type-annotated function add that takes a float a and a float b

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for floor function"""
+import math
+
+
+def floor(n: float) -> int:
+    """a type-annotated function which takes a float n as argument
+    and returns the floor of the float."""
+    return math.floor(n)
