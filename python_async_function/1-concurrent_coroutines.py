b9bd7a0d3b5caf7d4dc81900e8d45672e8aeafb9
MinutePapillon <christophebarrere@gmail.com>
2025-11-28 07:55:29 +0100

    Fix wait_n to run coroutines concurrently using asyncio.as_completed

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 """Module for the wait_n function"""
+import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """return the list of all the delays (float values)
-    sorted in ascending order"""
-    list = []
-    for i in range(n):
-        delay = await wait_random(max_delay)
-        list.append(delay)
-    return sorted(list)
+    """Execute wait_random n times concurrently and return delays sorted"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = []
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
